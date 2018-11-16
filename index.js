b2980a6d0cde237f342dc4a185791f5741dd1074
Liam Dawson <liam@ldaws.com>
2018-11-17 00:17:58 +1100

    partially rebuilt index

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
-const fs = require('fs');
-const toml = require('@iarna/toml');
-const Handlebars = require('handlebars');
+const fs = require("fs");
+const toml = require("@iarna/toml");
+const Handlebars = require("handlebars");
 
-const templateSource = fs.readFileSync('index.template.html', "utf8");
+const templateSource = fs.readFileSync("index.html", "utf8");
 
-const data = toml.parse(fs.readFileSync('data.toml', 'utf8'));
+const ctx = toml.parse(fs.readFileSync("data.toml", "utf8"));
 
-const itemHasCategory = (category) => (item) => (item.categories || []).indexOf(category) !== -1;
+const languageCodes = ["en"];
 
-Handlebars.registerHelper('itemsByCategory', function (category) {
-  const allItems = (data.items || []);
-  return allItems.filter(itemHasCategory(category));
+const language = "en";
+
+Handlebars.registerHelper(
+  "i18n",
+  (obj, key) => obj && obj[language] && obj[language][key]
+);
+
+const itemHasCategory = category => item =>
+  (item.categories || []).indexOf(category) !== -1;
+
+Handlebars.registerHelper("eachItemInCategory", function(category, options) {
+  const items = ctx.items.filter(
+    item => item.categories && item.categories.indexOf(category) !== -1
+  );
+
+  if (items && items.length) {
+    return items
+      .map(options.fn)
+      .reduce((a, b) => a + b);
+  }
+
+  return "";
 });
 
-fs.writeFileSync('build/en/index.html', Handlebars.compile(templateSource)(data));
+Handlebars.registerHelper("stringify", (obj) => JSON.stringify(obj, undefined, 2));
+
+languageCodes.forEach(code => {
+  fs.writeFileSync(
+    "build/" + code + "/index.html",
+    Handlebars.compile(templateSource)(ctx)
+  );
+});
